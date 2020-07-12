f47b8d4b4f23bde6e264a0f17d2d30d0e10ee07c
Bruno  de  Souza Picinini <brunosouzapicinini@gmail.com>
2020-07-12 13:28:29 -0300

    Implementacao de ListView e Mensagem de Alerta(toast)

--- TestDrive/TestDrive/MainPage.xaml.cs
+++ TestDrive/TestDrive/MainPage.xaml.cs
@@ -1,17 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace TestDrive
 {
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public string PrecoFormatado => $"R$ {Preco}";
+    }
+
     public partial class MainPage : ContentPage
     {
+        public List<Veiculo> Veiculos { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            Veiculos = new List<Veiculo>()
+            {
+                new Veiculo {Nome = "Azera V6", Preco = 60000},
+                new Veiculo {Nome = "Fiesta 2.0", Preco = 50000},
+                new Veiculo {Nome = "HB20 S", Preco = 40000}
+            };
+
+            BindingContext = this;
+        }
+
+        private void listViewVeiculos_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = e.Item as Veiculo;
+
+            DisplayAlert("Test Drive",
+                $"Você tocou no modelo {veiculo?.Nome}, que custa {veiculo?.PrecoFormatado} ",
+                "OK");
         }
     }
 }
