ad20b40a1f66230bfb42f72f4e5efb3695502579
Bruno  de  Souza Picinini <brunosouzapicinini@gmail.com>
2020-07-13 00:50:18 -0300

    Implementacao Tela de Agendamento

--- TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,13 +12,29 @@ namespace TestDrive.Views
     public partial class AgendamentoView : ContentPage
     {
         public Veiculo Veiculo { get; set; }
-        
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataAgendamento { get; set; } = DateTime.Today;
+        public TimeSpan HoraAgendamento { get; set; }
+
         public AgendamentoView(Veiculo veiculo)
         {
             InitializeComponent();
 
             Veiculo = veiculo;
             BindingContext = this;
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", 
+                $@"Nome: {Nome}
+Fone: {Fone}
+E-mail: {Email}
+Data Agendamento: {DataAgendamento:dd/MM/yyyy}
+Hora Agendamento: {HoraAgendamento}"
+                , "Ok");
         }
     }
 }
--- TestDrive/TestDrive/Views/DetalheView.xaml.cs
+++ TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -12,46 +12,46 @@ namespace TestDrive.Views
         private const int AR_CONDICIONADO = 1000;
         private const int MP3_PLAYER = 500;
 
-        public string TextoFreioABS => $"Freio ABS - R$ {FREIO_ABS}";
+        public string TextoFreioAbs => $"Freio ABS - R$ {FREIO_ABS}";
         public string TextoArCondicionado => $"Ar Condicionado - R$ {AR_CONDICIONADO}";
         public string TextoMp3Player => $"MP3 Player - R$ {MP3_PLAYER}";
 
         public string ValorTotal =>
-            $"Valor Total: R${Veiculo.Preco + (temFreioABS ? FREIO_ABS : 0) + (temArCondicionado ? AR_CONDICIONADO : 0) + (temMP3Player ? MP3_PLAYER : 0)}";
+            $"Valor Total: R${Veiculo.Preco + (_temFreioAbs ? FREIO_ABS : 0) + (_temArCondicionado ? AR_CONDICIONADO : 0) + (_temMp3Player ? MP3_PLAYER : 0)}";
 
-        private bool temFreioABS;
+        private bool _temFreioAbs;
 
-        public bool TemFreioABS
+        public bool TemFreioAbs
         {
-            get { return temFreioABS; }
+            get { return _temFreioAbs; }
             set
             {
-                temFreioABS = value;
+                _temFreioAbs = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(ValorTotal));
             }
         }
 
-        private bool temArCondicionado;
+        private bool _temArCondicionado;
 
         public bool TemArCondicionado
         {
-            get { return temArCondicionado; }
+            get { return _temArCondicionado; }
             set
             {
-                temArCondicionado = value;
+                _temArCondicionado = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(ValorTotal));
             }
         }
 
-        private bool temMP3Player;
-        public bool TemMP3Player
+        private bool _temMp3Player;
+        public bool TemMp3Player
         {
-            get { return temMP3Player; }
+            get { return _temMp3Player; }
             set
             {
-                temMP3Player = value;
+                _temMp3Player = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(ValorTotal));
             }
